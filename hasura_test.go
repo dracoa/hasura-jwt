20ff100c249f39ea4c913d82e1ba9ca0b281f276
Draco CHAU <draco.chau@cityu.edu.hk>
2020-04-24 10:46:55 +0800

    validate test

--- hasura_test.go
+++ hasura_test.go
@@ -14,10 +14,11 @@ type TestUser struct {
 
 func TestValidate(t *testing.T) {
 	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJtdW5nY2hhdSIsInJvbGUiOiJhZG1pbiIsInVzZXIiOnsiaWQiOiJtdW5nY2hhdSIsIm5hbWUiOiJEcmFjbyBDaGF1IiwiZW1haWwiOiJkY0BhYmMuY29tIiwicm9sZXMiOlsiYWRtaW4iXX0sImh0dHBzOi8vaGFzdXJhLmlvL2p3dC9jbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJhZG1pbiJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJhZG1pbiIsIngtaGFzdXJhLXVzZXItaGFsbHMiOlsiU1IwMSIsIlNSMDIiXSwieC1oYXN1cmEtdXNlci1pZCI6Im11bmdjaGF1IiwieC1oYXN1cmEtdXNlci1yb2xlcyI6ImFkbWluIiwieC1oYXN1cmEtdXNlci1zdXBlciI6dHJ1ZX0sImV4cCI6MTU4NzY5OTM3MiwiaWF0IjoxNTg3Njk1NzEyLCJpc3MiOiJIQVNVUkEtSldULUdFTkVSQVRPUiJ9.Mkz0vleXzuDlc4UkOjz4pTHDZBe2FcF_Z4IjSQ9Z-og"
-	err := Validate(token)
+	claims, err := Validate(token)
 	if err != nil {
 		t.Errorf("error while validate token: %v", err)
 	}
+	t.Log(claims)
 }
 
 func TestGenerate(t *testing.T) {
